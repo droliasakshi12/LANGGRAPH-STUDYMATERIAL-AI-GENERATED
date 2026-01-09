fb5321c878b24ac4cd3eae27fe72129b14ff8aad
droliasakshi12 <149218655+droliasakshi12@users.noreply.github.com>
2026-01-09 13:35:53 +0530

    Update road_map.py

--- pages/road_map.py
+++ pages/road_map.py
@@ -22,7 +22,7 @@ elif st.session_state['register'] == True:
     os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGCHAIN_TRACING_V2") or st.secrets.get["LANGCHAIN_TRACING_V2"]
     os.environ["LANGCHAIN_ENDPOINT"] = os.getenv("LANGCHAIN_ENDPOINT") or st.secrets.get["LANGCHAIN_ENDPOINT"]
     os.environ["LANGCHAIN_API_KEY"] = os.getenv("langsmith_api") or st.secrets.get["langsmith_api"]
-    os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT") or st.secrets["LANGCHAIN_PROJECT"]
+    os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT") or st.secrets.get["LANGCHAIN_PROJECT"]
 
     # getting models
     # model = ChatOpenAI(model="gpt-4.1-nano-2025-04-14")
@@ -179,5 +179,6 @@ elif st.session_state['register'] == True:
 
 else:
     st.switch_page("password.py")
+
 
 
