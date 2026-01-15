aaf4a27427868e572c619d2e6de9c510bbc56015
droliasakshi12 <149218655+droliasakshi12@users.noreply.github.com>
2026-01-15 12:39:20 +0530

    Update final_material.py

--- pages/final_material.py
+++ pages/final_material.py
@@ -9,6 +9,7 @@ import streamlit as st
 from docx import Document
 from pages.model_selection import selecting_model
 import io
+import time 
 
 if 'register' not in st.session_state:
     st.session_state['register'] = False
@@ -16,16 +17,18 @@ if 'register' not in st.session_state:
 elif st.session_state['register'] == True:
     load_dotenv(dotenv_path=r"my_api_key.env")
     os.environ["OPENAI_API_KEY"] = os.getenv("openai_api")
+    
+  
 
     # using openai model
     # -----------------------model selection --------------------
     evaluate_model = ChatOpenAI(model="gpt-3.5-turbo")
     optimized_model = ChatOpenAI(model="gpt-5-nano-2025-08-07")
-
+    st.title(" 👨‍💻Study Material Generation")
+    st.space("small")
+    
     models = selecting_model()
     model = ChatOpenAI(model=models)
-    st.sidebar.warning(
-        "NOTE - For better results use Model : 'gpt-4.1-mini','gpt-3.5-turbo','gpt-5-mini'")
 
     # ---------------------------------------------------------------
     # creating a pydantic model to evalaute the content
@@ -44,6 +47,7 @@ elif st.session_state['register'] == True:
 
     class studystate(TypedDict):
         topic: str = Field(description="topic for the material")
+        prompt : str = Field(description="prompt for generation")
         material: str = Field(description="study notes for the topic")
         evaluate: Literal["approved", "not_approved"]
         feedback: str = Field(description="feedback for the evaluation")
@@ -59,36 +63,20 @@ elif st.session_state['register'] == True:
 
     def study_material(state:  studystate):
         prompt = f"""You are an experienced professional educator and tutor.
-            Write a structured, theory-focused educational article on {state['topic']}.
-            Requirements:
-            - Explain concepts clearly with descriptive theoretical depth, progressing from basics to key ideas,maintain the balance of theoretical and practical knowledge. 
-            - Organize content into clear sections with headings.
-            - the content generated must be static do not change the format.
-            - In each section: (follow the points mentioned below) 
-            • Provide paragraph-based theory explanations.
-            • Include concise real-world examples with outcomes.(codes if required)
-            • Add  5-10 descriptive practice questions  along with code outputs compulsory at the end of the content.
-            - End with links for study resources relevant to the topic and beginner friendly websites(like w3schools,geeksforgeeks,etc.)
-            - provide the links that exists.
-            - Academic yet simple tone.
-            - No visuals, emojis, filler, or repetition.
-            - Be concise, accurate, and educational.
-            - Ensure to make the use of seo keywords in the middle of the material.            
+            Write a structured, theory-focused educational Study Material on {state['topic']}.
+            Based on the given prompt{state['prompt']} you have to genrate the final output.         
         """
-
+        
         response = model.invoke(prompt).content
-
-        return {"material": response, "material_history": [response]}
+        return {"material": response, "material_history": [response] , 'prompt': response}
 
     def evaluate_material(state: studystate):
         prompt = f"you have to evaluate the material and improve the content on the basis of the  {state['material']} , on topic {state['topic']}"
 
         response = struct_model.invoke(prompt)
-
         return {"evaluate": response.evaluate, "feedback": response.feedback, "feedback_history": response.feedback}
 
     # optimizing the content using the llm
-
     def optimize(state: studystate):
         prompt = f"optimize the content on topic {state['topic']} , material {state['material']}  and on the basis of the given feedback {state['feedback']} improve the material as and when required."
 
@@ -104,20 +92,47 @@ elif st.session_state['register'] == True:
             return "not_approved"
 
     # ---------------------------------------------------------------------
+   
     # uploading files
+    st.subheader("Upload Your File Below👇")
+    st.caption("✅Make sure The File Must Be Docx File")
     uploading_file = st.file_uploader("choose the file")
+  
     final_text = []
+    try:
+        if uploading_file is not None:
+            doc = Document(uploading_file)
 
-    if uploading_file is not None:
-        doc = Document(uploading_file)
-
-        for para in doc.paragraphs:
-            final_text.append(para.text)
-    else:
-        st.warning("please upload a file first")
+            for para in doc.paragraphs:
+                final_text.append(para.text)
+        else:
+            st.warning("please upload a file first 🗃️")
+            
+    except Exception as e :
+        print(f"Error {e}")
 
     content = "\n".join(final_text)
     st.markdown(content)
+    st.space("small")
+    st.subheader("✍️Prompting")
+    st.caption("you can edit the prompt as per your requirements")
+    prompt = st.text_area(label="Enter the prompt:",value='''You are an experienced professional educator and tutor.
+            Write a structured, theory-focused educational Study Material.
+            Requirements:
+            - Explain concepts clearly with descriptive theoretical depth, progressing from basics to key ideas,maintain the balance of theoretical and practical knowledge. 
+            - Organize content into clear sections with headings.
+            - the content generated must be static do not change the format.
+            - In each section: (follow the points mentioned below) 
+            • Provide paragraph-based theory explanations.
+            • Include concise real-world examples with outcomes.(codes if required)
+            • Add  5-10 descriptive practice questions  along with code outputs compulsory at the end of the content.
+            - End with links for study resources relevant to the topic and beginner friendly websites(like w3schools,geeksforgeeks,etc.)
+            - provide the links that exists.
+            - Academic yet simple tone.
+            - No visuals, emojis, filler, or repetition.
+            - Be concise, accurate, and educational.
+            - Ensure to make the use of seo keywords in the middle of the material.''',
+            height=500,width='stretch')
 
     # creating and compiling the graph
     graph = StateGraph(studystate)
@@ -135,25 +150,28 @@ elif st.session_state['register'] == True:
 
     # compiling the graph
     workflow = graph.compile()
-
+    
+   
     initial_state = {
-        "topic": content,
+        "topic" :content,
+        'prompt': prompt,
         "iteration": 1,
         "max_iteration": 3
     }
 
-    generate = st.button(label="GENERATE FINAL MATERIAL",
-                         use_container_width=True)
-    if generate:
-        final_output = workflow.invoke(initial_state)
-        final_material = final_output['material']
-        st.markdown(final_material)
+    generate = st.button(label="GENERATE FINAL MATERIAL",use_container_width=True)
 
-        st.session_state['final_material'] = final_material
+    if generate:
+        with st.spinner("⛷️Generating Response...."):
+            time.sleep(5)
+            final_output = workflow.invoke(initial_state)
+            final_material = final_output['material']
+            st.markdown(final_material)
+
+            st.session_state['final_material'] = final_material
 
     # inserting the data into the file
-    insert_in_file = st.button(
-        "INSERT MATERIAL INTO FILE", use_container_width=True)
+    insert_in_file = st.button("INSERT MATERIAL INTO FILE", use_container_width=True)
 
     if insert_in_file:
         final_material_filename = uploading_file.name.replace(".docx", "")
@@ -174,7 +192,7 @@ elif st.session_state['register'] == True:
             bio = io.BytesIO()
             doc.save(bio)
             bio.seek(0)
-            st.success("Data inserted successfully!!")
+            st.success("📝Data inserted successfully!!")
 
             # creating a download button to download the file
             st.download_button(
@@ -200,6 +218,3 @@ elif st.session_state['register'] == True:
 
 else:
     st.switch_page("password.py")
-
-
-
